01c72b8b54c4f15450866e1aa697ae3b432e38b8
amine-kt <amine.tifour.1973@gmail.com>
2021-05-18 00:10:24 +0200

    rangement de dossier/fichier V0.2

--- flux/upload.php
+++ flux/upload.php
@@ -8,7 +8,8 @@ $file_err = NULL;
 
 if (isset($_FILES['file']['name'])) {
 
-    $location = "./images/$id_user/picture_profile/{$_FILES['file']['name']}"; // Destination
+    $location = "../images/$id_user/picture_profile/{$_FILES['file']['name']}"; // Destination
+    $location2 = "./images/$id_user/picture_profile/{$_FILES['file']['name']}"; // Destination
     $filepath = strtolower(pathinfo($location, PATHINFO_EXTENSION));
 
     if ($filepath != "jpg" && $filepath != "png" && $filepath != "jpeg") { // Vérifie si le forma de l'image est différent de jpg png ou jpeg
@@ -22,7 +23,7 @@ if (isset($_FILES['file']['name'])) {
     //  Vérification extension du fichier
     if ($file_err === NULL) {
 
-        $files = glob("./images/$id_user/picture_profile/*"); // Récupère tout les fichier à l'emplacemenet ciblé
+        $files = glob("../images/$id_user/picture_profile/*"); // Récupère tout les fichier à l'emplacemenet ciblé
         foreach ($files as $file) { // parcours des fichier
             if (is_file($file)) { // Si il y a fichier
                 unlink($file); // Suprime les fichier
@@ -30,10 +31,10 @@ if (isset($_FILES['file']['name'])) {
         }
 
         if (move_uploaded_file($_FILES['file']['tmp_name'], $location)) { // SI l'envoie du fichier se fait à la destination
-            $_SESSION['user']['picture_profile'] = $location; // Met a jours la session picture_profile de l'user avec le chemin de destination du fichier
+            $_SESSION['user']['picture_profile'] = $location2; // Met a jours la session picture_profile de l'user avec le chemin de destination du fichier
             $sql = "UPDATE user SET picture_profile='{$_SESSION['user']['picture_profile']}' WHERE id_user='{$_SESSION['user']['id_user']}'"; // Prépare la requête slq
             $db->query($sql); // Envoie à la bdd
-            echo json_encode(['success' => true, 'file_err' => $file_err, 'picture_profile' => $location]);
+            echo json_encode(['success' => true, 'file_err' => $file_err, 'picture_profile' => $location2]);
             die();
         } else {
             $file_err = "Une erreur innatendue est survenue";
